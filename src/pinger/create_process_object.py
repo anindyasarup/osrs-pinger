25a7abac938512afb815827033ae84164d99de55
Anindya Sarup <anindyasarup@github.com>
2023-08-20 19:30:09 +1000

    refractor process object creation file

--- src/pinger/create_process_object.py
+++ src/pinger/create_process_object.py
@@ -1,7 +1,6 @@
 """ 
-Module providing function for creating a new process 
-and help interact with the input and output streams 
-of the process. 
+Module providing function for creating a new process and help interact 
+with the input and output streams of the process. 
 """
 from typing import Union, List
 import subprocess
@@ -9,15 +8,16 @@ import subprocess
 
 def create_process_object(logger, process_args: Union[str, List[str]]) -> subprocess.Popen[bytes]:
     """
-    Create Process Object with the given process arguments using subprocess module
+    Create Process Object with the given process arguments using 
+    subprocess module
 
     Args:
         process_args (Union[str, List[str]]): a string command or 
             a string array with command and command line arguments
 
     Returns:
-        subprocess.Popen[bytes]: process object setup with the given process 
-            arguments 
+        subprocess.Popen[bytes]: process object setup with the given 
+            process arguments 
     """
     try:
         logger.info("Creating process Object")
