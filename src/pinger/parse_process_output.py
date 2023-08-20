5bb88d56691627eda06b9f426b905502d64bd1a4
Anindya Sarup <anindyasarup@github.com>
2023-08-20 19:13:45 +1000

    add python function to process object output

---
+++ src/pinger/parse_process_output.py
@@ -0,0 +1,7 @@
+def parse_process_output(process_out: str):
+    lines = process_out.strip().split('\n')
+    last_line = lines[-1]
+    avg_rtt = float(last_line.split('/')[-3])
+    max_rtt = float(last_line.split('/')[-2])
+
+    return avg_rtt, max_rtt
